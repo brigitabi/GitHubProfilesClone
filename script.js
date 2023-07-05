bf156abe0c9130111a1518c725bc457b65e730cf
Brigita Bizjak <brigita.bizjak05@gmail.com>
2023-07-05 12:36:15 +0200

    creating user card fuction and event listener

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const APIURL = 'https://api.github.com/users'; 
 
 const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
 
-async function getUser() { 
-    const resp = await(APIURL + user);
+async function getUser(user) { 
+    const resp = await fetch(APIURL + user);
     const respData = await resp.json();
 
     createUserCard(respData);
@@ -11,22 +13,35 @@ async function getUser() {
 }
 
 function createUserCard(user) { 
-    const card = document.createElement('div');
-    card.classList.add('card');
+    const cardHTML = `
+        <div class="card">
+            <div>
+                <img src="${user.avatar_url}" alt="${user.name}" />
+            </div>
+            <div>
+                <h2>${user.name}</h2>
+                <p>${user.bio}</p>
 
-    card.innerHTML = `
+                <ul>
+                    <li>${user.followers}</li>
+                    <li>${user.following}</li>
+                    <li>${user.public_repos}</li>
+                </ul>
+            </div>
         <div>
-            <img src="${user.avatar_url}" alt="${user.name}" />
-        </div>
-        <div>
-            <h2>${user.name}</h2>
-            <p>${user.bio}</p>
-
-            <ul>
-                <li>${user.followers}</li>
-                <li>${user.following}</li>
-                <li>${user.public_repos}</li>
-            </ul>
-        </div>
     `;
+
+    main.innerHTML = cardHTML; 
 }
+
+form.addEventListener("submit", (e) => { 
+    e.preventDefault();
+
+    const user = search.value;
+
+    if (user) { 
+        getUser(user); 
+
+        search.value = ""; 
+    }
+});
